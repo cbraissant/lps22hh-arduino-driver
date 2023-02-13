bc66f957a9c9faf50572c07ed86a80af4e9313cd
Chris Braissant <chrisbraissant@gmail.com>
2023-02-13 16:26:49 +0100

    First proof of concept, new to be written properly

--- lib/LPS22/lps22.h
+++ lib/LPS22/lps22.h
@@ -10,35 +10,64 @@
 /** Registers definition
  */
 #define LPS_INTERRUPT_CFG 0x0B  ///< Interrupt mode register
-#define LPS_THS_P_L 0x0C ///< Pressure threshold registers (LSB)
-#define LPS_THS_P_H 0x0D ///< Pressure threshold registers (MSB)
-#define LPS_IF_CTRL 0x0E ///< Interface control register
-#define LPS_WHO_AM_I 0x0F ///< Who am I
+#define LPS_THS_P_L 0x0C        ///< Pressure threshold registers (LSB)
+#define LPS_THS_P_H 0x0D        ///< Pressure threshold registers (MSB)
+#define LPS_IF_CTRL 0x0E        ///< Interface control register
+#define LPS_WHO_AM_I 0x0F       ///< Who am I
 
-#define LPS_CTRL_REG1 0x10 ///< Control register (LSB)
-#define LPS_CTRL_REG2 0x11 ///< Control register
-#define LPS_CTRL_REG3 0x12 ///< Control register (MSB)
+#define LPS_CTRL_REG1 0x10      ///< Control register (LSB)
+#define LPS_CTRL_REG2 0x11      ///< Control register
+#define LPS_CTRL_REG3 0x12      ///< Control register (MSB)
 
-#define LPS_FIFO_CTRL 0x13 ///< FIFO configuration register
-#define LPS_FIFO_WTM 0x14 ///< FIFO Threshold setting register
-#define LPS_REF_P_L 0x15 ///< Reference pressure register (LSB)
-#define LPS_REF_P_H 0x16 ///< Reference pressure register (MSB)
-#define LPS_RPDS_L 0x18 ///< Pressure offset register (LSB)
-#define LPS_RPDS_H 0x19 ///< Pressure offset register (MSB)
+#define LPS_FIFO_CTRL 0x13      ///< FIFO configuration register
+#define LPS_FIFO_WTM 0x14       ///< FIFO Threshold setting register
+#define LPS_REF_P_L 0x15        ///< Reference pressure register (LSB)
+#define LPS_REF_P_H 0x16        ///< Reference pressure register (MSB)
+#define LPS_RPDS_L 0x18         ///< Pressure offset register (LSB)
+#define LPS_RPDS_H 0x19         ///< Pressure offset register (MSB)
 
-#define LPS_INT_SOURCE 0x24 ///< Interrupt register
-#define LPS_FIFO_STATUS1 0x25 ///< FIFO status register
-#define LPS_FIFO_STATUS2 0x26 ///< FIFO status register
-#define LPS_STATUS 0x27 ///< Status register
+#define LPS_INT_SOURCE 0x24     ///< Interrupt register
+#define LPS_FIFO_STATUS1 0x25   ///< FIFO status register
+#define LPS_FIFO_STATUS2 0x26   ///< FIFO status register
+#define LPS_STATUS 0x27         ///< Status register
 
-#define LPS_PRESSURE_OUT_XL 0x28  ///< Pressure output register (LSB)
-#define LPS_PRESSURE_OUT_L 0x29   ///< Pressure output register
-#define LPS_PRESSURE_OUT_H 0x2A   ///< Pressure output register (MSB)
-#define LPS_TEMP_OUT_L 0x2B   ///< Temperature output register (LSB)
-#define LPS_TEMP_OUT_H 0x2C   ///< Temperature output register (LSB)
+#define LPS_PRESSURE_OUT_XL 0x28    ///< Pressure output register (LSB)
+#define LPS_PRESSURE_OUT_L 0x29     ///< Pressure output register
+#define LPS_PRESSURE_OUT_H 0x2A     ///< Pressure output register (MSB)
+#define LPS_TEMP_OUT_L 0x2B         ///< Temperature output register (LSB)
+#define LPS_TEMP_OUT_H 0x2C         ///< Temperature output register (LSB)
 
 #define LPS_FIFO_DATA_OUT_PRESS_XL 0x78 ///< FIFO pressure output register
 #define LPS_FIFO_DATA_OUT_PRESS_L 0x79  ///< FIFO pressure output register
 #define LPS_FIFO_DATA_OUT_PRESS_H 0x7A  ///< FIFO pressure output register
-#define FIFO_DATA_OUT_TEMP_L 0x7B // FIFO temperature output registers
-#define FIFO_DATA_OUT_TEMP_H 0x7C // FIFO temperature output registers
+#define LPS_FIFO_DATA_OUT_TEMP_L 0x7B   ///< FIFO temperature output registers
+#define LPS_FIFO_DATA_OUT_TEMP_H 0x7C   ///< FIFO temperature output registers
+
+/** Characteristics
+*/
+#define LPS_MIN_PRESSURE 260    // hPa
+#define LPS_MAX_PRESSURE 1260   // hPa
+#define LPS_PRESSURE_SENSITIVITY 4096       // 4096 LSB = 1 hPa
+#define LPS_PRESSURE_RESOLUTION 0.0002441406   // 1 LSB = 1/4096 = 0.0002441406 hPa
+#define LPS_TEMPERATURE_SENSITIVITY 100     // 100 LSB = °C
+#define LPS_TEMPERATURE_RESOLUTION  0.01    // 1 LSB = 1/100 = 0.01 °C
+#define LPS_SPI_CLOCK_FREQUENCY 1e6         // 1 Mhz
+
+/**
+ * @brief
+ * 
+ * Output data rate bit configurations
+*/
+typedef enum{
+    LPS_ODR_ONE_SHOT,
+    LPS_ODR_1_HZ,
+    LPS_ODR_10_HZ,
+    LPS_ODR_25_HZ,
+    LPS_ODR_50_HZ,
+    LPS_ODR_75_HZ,
+    LPS_ODR_100_HZ,
+    LPS_ODR_200_HZ
+} lps_odr;
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,111 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <LPS22.h>
+
+const int CS_pin = 10; // Chip Select pin for the LPS22HH
+
+byte pressure_xl, pressure_l, pressure_h; // Variables to store pressure data
+byte temp_l, temp_h; // Variables to store temperature data
+
+int readRegister(int register_address) {
+  digitalWrite(CS_pin, LOW);
+  SPI.transfer(register_address | 0x80);
+  int data = SPI.transfer(0x00);
+  digitalWrite(CS_pin, HIGH);
+  return data;
+}
+
+void writeRegister(int register_address, int register_value) {
+  digitalWrite(CS_pin, LOW);
+  SPI.transfer(register_address);
+  SPI.transfer(register_value);
+  digitalWrite(CS_pin, HIGH);
+}
+
+
+
+/**
+ * @brief
+ * 
+ * Set the output data rate
+ * 
+ * @param output_data_rate value of the Output data rate bit configurations 
+*/
+void setDataRate(lps_odr output_data_rate){
+  // read the actual configuration of the register
+  int controlRegister = readRegister(LPS_CTRL_REG1);
+  Serial.print("Actual control register: ");
+  Serial.println(controlRegister);
+
+  // change the data rate
+  controlRegister |= output_data_rate << 4;
+  Serial.print("Futur control register: ");
+  Serial.println(controlRegister);
+  
+  // write the new configuration to the register
+  writeRegister(LPS_CTRL_REG1, controlRegister);
+
+  // read the configuration to confirm
+  controlRegister = readRegister(LPS_CTRL_REG1);
+  Serial.print("New control register: ");
+  Serial.println(controlRegister);
+
+  // Disable SPI communication
+  digitalWrite(CS_pin, LOW); 
+}
+
+
+
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println("Programm started");
+
+  SPI.begin();
+
+  pinMode(CS_pin, OUTPUT);
+  digitalWrite(CS_pin, HIGH);
+
+  // Initialize the SPI settings
+  SPI.beginTransaction(SPISettings(LPS_SPI_CLOCK_FREQUENCY, MSBFIRST, SPI_MODE3));
+
+  // Read the WHO_AM_I register
+  byte who_am_i = readRegister(LPS_WHO_AM_I);
+  Serial.print("Who Am I? ");
+  Serial.println(who_am_i);
+
+  // Change the Output data rate
+  setDataRate(LPS_ODR_10_HZ);
+
 }
 
 void loop() {
+  // Read the temperature
+  temp_l = readRegister(LPS_TEMP_OUT_L);
+  temp_h = readRegister(LPS_TEMP_OUT_H);
+
+  int32_t temperature = (temp_h << 8) | temp_l;
+
+  Serial.print("Temp: ");
+  Serial.println(temperature*LPS_TEMPERATURE_RESOLUTION);
+
+  // Read the 3 bytes of the pressure
+  pressure_xl = readRegister(LPS_PRESSURE_OUT_XL); // Read the pressure low byte
+  pressure_l = readRegister(LPS_PRESSURE_OUT_L); // Read the pressure medium byte
+  pressure_h = readRegister(LPS_PRESSURE_OUT_H); // Read the pressure high byte
+
+  // Concatenate the pressures
+  int32_t pressure;
+  pressure = (int32_t)(pressure_h);
+  pressure <<= 8;
+  pressure |= (int32_t)(pressure_l);
+  pressure <<= 8;
+  pressure |= (int32_t)(pressure_xl);
+
+
+  Serial.print("Pressure: ");
+  Serial.println(pressure*LPS_PRESSURE_RESOLUTION);
+
+  // Repeat the loop
+  delay(1000);
 }
